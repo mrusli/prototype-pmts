87642d9d9cd8e5f5e66512bcd41a8984f5571e0d
mrusli <mrusli@asimetris.co.id>
2023-07-26 14:56:08 +0700

    Bug Fix 403 - Update SecurityConfig.java

--- src/main/java/mil/pusdalops/webui/security/SecurityConfig.java
+++ src/main/java/mil/pusdalops/webui/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package mil.pusdalops.webui.security;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,6 +11,17 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 	private static final Logger log = Logger.getLogger(SecurityConfig.class);
+
+	private static final String ZUL_FILES = "/zkau/web/plbn/**";
+	
+    private static final String[] ZK_RESOURCES = {
+            "/zkau/web/js/*",
+            "/zkau/web/css/*",
+            "/zkau/web/font/*",
+            "/zkau/web/img/*",
+            "/zkau/web/error/*"
+    };
+    private static final String REMOVE_DESKTOP_REGEX = "/zkau\\?dtid=.*&cmd_0=rmDesktop&.*";
 
 	public SecurityConfig() {
 		super();
@@ -23,7 +34,16 @@ public class SecurityConfig {
     	log.info("Define Security FilterChain - Disable the default spring security login");
     	
     	http
-    		.httpBasic(Customizer.withDefaults());
+    		.csrf(csrf -> csrf.disable())
+        	.authorizeHttpRequests(authz -> authz
+        		.requestMatchers(ZUL_FILES).permitAll()
+        		.requestMatchers("/**").permitAll()
+    			.requestMatchers(HttpMethod.GET, ZK_RESOURCES).permitAll()
+    			.requestMatchers(HttpMethod.GET, REMOVE_DESKTOP_REGEX).permitAll()
+    			.requestMatchers(req -> "rmDesktop".equals(req.getParameter("cmd_0"))).permitAll());
+    			// .requestMatchers("/success").authenticated()
+    			// .requestMatchers("/secure/**").authenticated()
+    			// .anyRequest().authenticated())
  		
     	return http.build();
     }	
